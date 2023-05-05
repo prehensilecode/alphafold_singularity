44bd80ccbf26220df91d9e47fb9db91aaec4564f
David Chin <prehensilecode@users.noreply.github.com>
2023-05-05 17:37:16 -0400

    Update for AlphaFold 2.3.2. Reset author email.

--- example_slurm_job.sh
+++ example_slurm_job.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-#SBATCH -p gpu
-#SBATCH --time=18:00:00
+#SBATCH --partition=gpu
+#SBATCH --time=2:00:00
 #SBATCH --gpus=4
 #SBATCH --cpus-per-gpu=12
 #SBATCH --mem=140G
 
-module load alphafold/2.2.4
-module load python/gcc/3.10
+### NOTE
+#### This job script cannot be used without modification for your specific environment.
+
+module load python/gcc/3.11
+module load alphafold/2.3.2
 
 ### Check values of some environment variables
+echo SLURM_GPUS_ON_NODE=$SLURM_GPUS_ON_NODE
 echo SLURM_JOB_GPUS=$SLURM_JOB_GPUS
+echo SLURM_STEP_GPUS=$SLURM_STEP_GPUS
 echo ALPHAFOLD_DIR=$ALPHAFOLD_DIR
 echo ALPHAFOLD_DATADIR=$ALPHAFOLD_DATADIR
 
 ###
-### README This runs AlphaFold 2.2.2 on the T1050.fasta file
+### README This runs AlphaFold 2.3.2 on the T1050.fasta file
 ###
 
 # AlphaFold should use all GPU devices available to the job by default.
@@ -30,12 +35,12 @@ echo ALPHAFOLD_DATADIR=$ALPHAFOLD_DATADIR
 
 # Run AlphaFold; default is to use GPUs, i.e. "--use_gpu" can be omitted.
 python3 ${ALPHAFOLD_DIR}/singularity/run_singularity.py \
-    --use_gpu --gpu_devices=${SLURM_JOB_GPUS} \
+    --use_gpu \
     --data_dir=${ALPHAFOLD_DATADIR} \
     --fasta_paths=T1050.fasta \
     --max_template_date=2020-05-14 \
-    --model_preset=monomer_casp14 \
-    --benchmark
+    --model_preset=monomer \
+    --db_preset=reduced_dbs
 
 echo INFO: AlphaFold returned $?
 
